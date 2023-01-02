6cbb9d9b500c5b969aee846e6aff4f1d2320357b
keelan <keelan.foley.2020@mumail.ie>
2023-01-02 21:01:24 +0000

    documentation updated

--- R/exchange.R
+++ R/exchange.R
@@ -1,13 +1,25 @@
-library(quantmod)
 
-#function to load in forex data
+
+#' function to load in Forex data
+#' @param rates_list A string or list of strings corresponding to Forex currency abbreviations
+#'
+#' @return data frame with historic exchange rates from last 180 days
+#' @export
+#'
+#' @importFrom quantmod "getFX"
+#'
+#' @examples
+#' dat1<- load_fx()
+#' dat2<-load_fx("EUR/USD")
+#' dat3<-load_fx(c("EUR/USD","EUR/GBP","EUR/AUD"))
+#'
 load_fx<- function(rates_list=c("EUR/USD","EUR/GBP","EUR/AUD")){
   start_date <- Sys.Date()-180
   end_date <- Sys.Date()
   rates_df <- NULL
   for (idx in seq(length(rates_list))){
     rate_index = rates_list[idx]
-    getFX(rate_index,
+    quantmod::getFX(rate_index,
           from=start_date,to=end_date)
     temp_df = as.data.frame(get(gsub("/","",rate_index)))
     temp_df$Date = row.names(temp_df)
@@ -19,7 +31,23 @@ load_fx<- function(rates_list=c("EUR/USD","EUR/GBP","EUR/AUD")){
   }
   return(rates_df)
 }
-#function to get exchange rates
+
+
+
+#' Load in exchange rate for a particular date from last 180 days
+#'
+#' @param base the currency you want to convert from as Forex currency abbreviation
+#' @param foreign the currency you want to convert to as Forex currency abbreviation
+#' @param date the date this of which you would like th exchange rate from
+#'
+#' @return the specified exchange rate
+#' @export
+#'
+#' @examples
+#'exchange_rate("EUR","AUD" ,"2022-10-03")
+#'exchange_rate("AUD","EUR" )
+#'exchange_rate("GBP","AUD")
+#'
 exchange_rate <- function(base,foreign,date=Sys.Date()-1){
   base<-toupper(base)
   foreign<-toupper(foreign)
@@ -31,7 +59,6 @@ exchange_rate <- function(base,foreign,date=Sys.Date()-1){
   rate<-dayrates[,"rate"]
   return(rate)
 }
-#few sample calls
-exchange_rate("EUR","AUD" ,"2022-10-03")
-exchange_rate("AUD","EUR" )
-exchange_rate("GBP","AUD")
+
+
+
